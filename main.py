087a48a26bd457c99128922e46b1cfa24ec9e27b
AzerimX <cezary.ziecina@gmail.com>
2022-10-05 20:43:09 +0200

    Finish function to scrap deck links from tier lists with tiers separated

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ def get_tier_list_links(mtg_format: str) -> pd.DataFrame:
         raise Exception('Format not on the list of formats: ', mtg_format)
 
     full_url = url + format_url
-    page = requests.get(full_url)
-    soup = BeautifulSoup(page.content, "html.parser")
+    page = requests.get(full_url)  # get the page
+    soup = BeautifulSoup(page.content, "html.parser")  # get only it's content
     d2b_element = soup.find(attrs={"name": "archive_d2b"})  # search for <eslect> tag
     d2b_options = d2b_element.find_all(name='option')  # get all <option>s from <select>
     month_link = []
@@ -77,5 +77,27 @@ def get_tier_list_links(mtg_format: str) -> pd.DataFrame:
     return decks_to_beat_months
 
 
-get_tier_list_links('modern')
+def get_deck_links(tier_list_url: str) -> pd.DataFrame:
+    """
+    :param tier_list_url: string that is a link to a tier list
+    :return: pd.DataFrame with links to all decks on that page
+    """
+    page = requests.get(tier_list_url)  # get the page
+    soup = BeautifulSoup(page.content, "html.parser")  # get only it's content
+    # this is spaghetti but the website is also spaghetti
+    searching_by_style = soup.find_all(style='margin:0px 4px 0px 4px;')
+    #print(searching_by_style)
+    for element in searching_by_style:  # it should be exactly 1 iteration any ways
+        asdjjj = element.find_all('tr')
+        # todo each <TR> is a tier on the tier list!
+        print(asdjjj)
+        # ahref = element.find_all('a')
+        # for element2 in ahref:
+        #     print(element2)
+    #asd = soup.find_all(attrs={'class': 'S14'})  # spaghetti but the website is also a pasta
+    #print(asd)
+
+
+modern_tier_lists = get_tier_list_links('modern')
+get_deck_links('https://www.mtgtop8.com/event?e=2552&f=MO')
 # todo scrapping tier list
